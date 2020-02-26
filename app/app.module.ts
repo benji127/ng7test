524b99d660cba8ccdb858e11ab3cfa867ee3fd88
benji127 <eneapaja27@gmail.com>
2020-02-26 10:45:01 +0100

    1 file modified, 3 added, 3 deleted

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,20 +5,18 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ProductListComponent } from './product-list/product-list.component';
 
 
-// used to create fake backend
-import { fakeBackendProvider } from './_helpers';
 
 import { AppComponent }  from './app.component';
 import { routing }        from './app.routing';
 
 import { AlertComponent } from './_components';
-import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { JwtInterceptor, ErrorInterceptor, fakeBackendProvider } from './_helpers';
 import { HomeComponent } from './home';
 import { LoginComponent } from './login';
 import { RegisterComponent } from './register';
 import { FootBarComponent } from './foot-bar/foot-bar.component';
 import { AdminComponent } from './admin/admin.component';
-import { ProductAlertsComponent } from './product-alerts/product-alert.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { CartComponent } from './cart/cart.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ShippingComponent } from './shipping/shipping.component';
